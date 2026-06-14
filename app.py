ff1793faca6b9d5e33bde0765544295104a8484c
Payal <ucc.payal04@gmail.com>
2026-06-14 17:23:34 +0530

    Add multiple feature to GitHub Actions pipeline

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
-
+def multiply(a, b):
+    return a * b
 if __name__ == "__main__":
     result1 = add(5, 3)
     result2 = subtract(10, 4)
     
     print(f"5 + 3 = {result1}")
     print(f"10 - 4 = {result2}")
+    result3 = multiply(6, 7)
+    print(f"6 * 7 = {result3}")
     print("✅ All calculations successful!")
+    
+    
